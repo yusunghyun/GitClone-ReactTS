6ef23a0dbf8c1e2bbb5eb2e272efed9b201bdd21
SungHyun Yu <ysh1513@naver.com>
2020-05-14 01:21:06 +0900

    CRA, 라이브러리 설치 화이팅!

---
+++ fron/src/App.tsx
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import Tes from './Tes'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Tes/>
+      </div>
+    );
+  }
+}
+
+export default App;
